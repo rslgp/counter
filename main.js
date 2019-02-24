c85ba2a7405ace58464ca1b6b98372ad27580b08
rslgp <rslgp@cin.ufpe.br>
2019-02-24 20:12:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ var discAutorizados, salasAutorizadas;
 
 client.on('ready', () => {
     app.startApp(client);
-	client.user.username="ReifelTracker";
-	client.user.setUsername("ReifelTracker");
+	client.user.username="ReifelContagem";
+	client.user.setUsername("ReifelContagem");
 	
 	client.channels.get("459432939898273798").fetchMessage('461722127205269505')
 			  .then(message => {
